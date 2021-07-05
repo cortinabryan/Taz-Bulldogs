dc83065cd4e9faac2a65f1acb9ef3b31070db83f
Bryan <cortinabryan@gmail.com>
2021-07-04 20:01:58 -0700

    transfering initial files + Navbar Component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,58 @@
+import React from "react";
+import logo from "../images/logo.png";
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <h1>test navbar</h1>
+    <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          <img src={logo} alt="" className="logo" />
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                About Us
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Available Puppies
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                FAQ's
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Photos
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
